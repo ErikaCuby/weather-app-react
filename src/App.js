96ffa484f14ce23e90c69810f820827a778d3269
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-04-16 01:08:18 +0200

    Moved Search code inside of Main and deleted both search files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Search from "./Search.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
 
@@ -7,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Search />
         <Main />
         <Footer />
       </div>
--- src/Main.js
+++ src/Main.js
@@ -3,22 +3,35 @@ import "./Main.css";
 
 export default function Main() {
   return (
-    <main>
-      <div className="current-weather">
-        <div className="city-info">
-          <h1>Paris</h1>
-          <p>
-            Saturday 15:32, moderate rain <br />
-            Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong>
-          </p>
+    <div>
+      <header>
+        <form>
+          <span className="enter-a-city">
+            <input type="search" placeholder="Enter a city.." required />
+          </span>
+          <span className="search">
+            <input type="submit" value="Search" />
+          </span>
+        </form>
+        <hr />
+      </header>
+      <main>
+        <div className="current-weather">
+          <div className="city-info">
+            <h1>Paris</h1>
+            <p>
+              Saturday 15:32, moderate rain <br />
+              Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong>
+            </p>
+          </div>
+          <div className="city-temperature">
+            <span className="weather-emoji">☀️</span>
+            <span className="temperature-value"> 24</span>
+            <span className="celsius">°C</span>
+          </div>
         </div>
-        <div className="city-temperature">
-          <span className="weather-emoji">☀️</span>
-          <span className="temperature-value"> 24</span>
-          <span className="celsius">°C</span>
-        </div>
-      </div>
-      <hr />
-    </main>
+        <hr />
+      </main>
+    </div>
   );
 }
